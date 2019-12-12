640d5b1822d37e951da4e250743f2488ce431563
anandijain <anandj@uchicago.edu>
2019-12-11 23:37:44 -0600

    basic http and json

--- main.go
+++ main.go
@@ -1,124 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 
-	"strings"
-
 	"github.com/gocolly/colly"
 	// "github.com/gocolly/colly/proxy"
 )
 
-// Sport : specifying game
-type Sport struct {
-	Competitions []Competition `json:""`
-}
 
-// Competition : specifying game
-type Competition struct {
-	Events []Event `json:"events"`
-	Paths  []Path  `json:"path"`
-}
 
-// Event : specifying game
-type Event struct {
-	id            string         `json:"id"`
-	description   string         `json:"description"`
-	eventType     string         `json:"type"`
-	link          string         `json:"link"`
-	status        string         `json:status`
-	sport         string         `json:"sport"`
-	startTime     int            `json:"startTime"`
-	live          bool           `json:"live"`
-	awayTeamFirst bool           `json:"awayTeamFirst"`
-	denySameGame  string         `json:"denySameGame"`
-	teaserAllowed bool           `json:"teaserAllowed"`
-	competitionID string         `json:"competitionId"`
-	notes         string         `json:"notes"`
-	numMarkets    int            `json:"numMarkets"`
-	lastModified  int            `json:"lastModified"`
-	competitors   []Competitor   `json:"competitors"`
-	displayGroups []DisplayGroup `json:"displayGroups"`
-}
-
-// DisplayGroup : specifying game
-type DisplayGroup struct {
-	id            string   `json: id`
-	description   string   `json: description`
-	defaultType   bool     `json: defaultType`
-	alternateType bool     `json: alternateType`
-	markets       []Market `json: markets`
-	order         int      `json: order`
-}
-
-// Competitor : specifying game
-type Competitor struct {
-	home bool   `json: home`
-	id   string `json: id`
-	name string `json: name`
-}
-
-// Market : specifying game
-type Market struct {
-	id           string    `json: id`
-	description  string    `json: description`
-	key          string    `json: key`
-	marketTypeID string    `json: marketTypeId`
-	status       string    `json: status`
-	singleOnly   bool      `json: singleOnly`
-	notes        string    `json: notes`
-	period       Period    `json: period`
-	outcomes     []Outcome `json: outcomes`
-}
-
-// Outcome : specifying game
-type Outcome struct {
-	id           string `json: id`
-	description  string `json: description`
-	status       string `json: status`
-	outcomeType  string `json: outcomeType`
-	competitorID string `json: competitorId`
-	price        Price  `json: price`
-}
-
-// Price : specifying game
-type Price struct {
-	id         string `json: id`
-	handicap   string `json: handicap`
-	american   string `json: american`
-	decimal    string `json: decimal`
-	fractional string `json: fractional`
-	malay      string `json: malay`
-	indonesian string `json: indonesian`
-	hongkong   string `json: hongkong`
-}
-
-// Period : specifying game
-type Period struct {
-	id           string `json: id`
-	description  string `json: description`
-	abbreviation string `json: abbreviation`
-	live         bool   `json: live`
-	main         bool   `json: main`
-}
-
-// Path : specifying path
-type Path struct {
-	id          string `json: id`
-	link        string `json: link`
-	description string `json: description`
-	pathType    string `json: pathType`
-	sportCode   string `json: sportCode`
-	order       int    `json: order`
-	leaf        bool   `json: leaf`
-	current     bool   `json: current`
-}
-
-func main() {
+func sips() {
 	fName := "bk_boxscores.csv"
 	file, err := os.Create(fName)
 
@@ -143,26 +39,32 @@ func main() {
 		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
 	)
 
-	detailCollector := c.Clone()
+	// detailCollector := c.Clone()
 
 	// c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 2})
 
 	c.OnRequest(func(r *colly.Request) {
 		log.Println("visiting", r.URL.String())
-		fmt.Println(r.Body)
+		// fmt.Println(r.Headers)
+		// fmt.Println(r.ID)
+		// fmt.Println(r.Marshal)
+		// fmt.Println(r.Ctx)
 	})
-
-	c.OnHTML("html", func(e *colly.HTMLElement) {
-		fmt.Println(e.ChildTexts("body"))
-	})
-	// On every a HTML element which has name attribute call callback
-	c.OnHTML(`a[id]`, func(e *colly.HTMLElement) {
-		// Activate detailCollector if the link contains "coursera.org/learn"
-		courseURL := e.Request.AbsoluteURL(e.Attr("href"))
-		if strings.Index(courseURL, "basketball-reference.com/boxscores/") != -1 {
-			detailCollector.Visit(courseURL)
-		}
-	})
+	// fmt.Println("this is C: ", c.String())
+	// c.OnHTML("body", func(e *colly.HTMLElement) {
+	// 	fmt.Println("e.Text: ", e.Text)
+	// })
+	// // On every a HTML element which has name attribute call callback
+	// c.OnHTML(`a[id]`, func(e *colly.HTMLElement) {
+	// 	// Activate detailCollector if the link contains "coursera.org/learn"
+	// 	courseURL := e.Request.AbsoluteURL(e.Attr("href"))
+	// 	writer.Write([]string{
+	// 		e.Request.AbsoluteURL(e.ChildAttr("a", "href")),
+	// 	})
+	// 	if strings.Index(courseURL, "basketball-reference.com/boxscores/") != -1 {
+	// 		detailCollector.Visit(courseURL)
+	// 	}
+	// })
 
 	// c.OnHTML("p[class=links]", func(e *colly.HTMLElement) {
 	// 	fmt.Println(e.Request.AbsoluteURL(e.ChildAttr("a", "href")))
@@ -172,21 +74,28 @@ func main() {
 	// 	fmt.Println(e.Request.AbsoluteURL(e.ChildAttr("a", "href")))
 	// })
 
-	c.OnHTML("html", func(e *colly.HTMLElement) {
-		fmt.Println("html")
-		dat := e.ChildText("body > pre")
-		fmt.Println(dat)
-		// jsonData := dat[strings.Index(dat, "{") : len(dat)-1]
-		data := &Sport{}
-		err := json.Unmarshal([]byte(dat), data)
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		fmt.Println("in pre")
+		fmt.Println("dom children: ", e.DOM.Children().Text())
+		jsonData := e.Text
+		fmt.Println("jsonData: ", err)
+		parsed := &ResultStruct{}
+		err := json.Unmarshal([]byte(jsonData), parsed)
+		fmt.Println("parsed: ", err)
+
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(data)
 	})
 
-	c.Visit("https://www.basketball-reference.com/boxscores/")
-	// c.Visit("https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl")
+	d := c.Clone()
+	d.OnResponse(func(r *colly.Response) {
+		idStart := bytes.Index(r.Body, []byte(`:n},queryId:"`))
+		requestID = string(r.Body[idStart+13 : idStart+45])
+	})
+
+	// c.Visit("https://www.basketball-reference.com/boxscores/")
+	c.Visit("https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl")
 
 	log.Println(c)
 }
--- sips.go
+++ sips.go
@@ -1,49 +1,162 @@
 package main
 
-import(
+import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
+	"strings"
 )
 
-type people struct {
-	Number int `json:"number"`
+// Sport : specifying game
+type Sport struct {
+	Competitions []Competition `json:""`
 }
 
-func requ() {
-	sport := "basketball/nba"
-	url := "https://www.bovada.lv/services/sports/event/v2/events/A/description/" + sport
+// Competition : specifying game
+type Competition struct {
+	Events []Event `json:"events"`
+	Paths  []Path  `json:"path"`
+}
 
-	spaceClient := http.Client{
-		Timeout: time.Second * 2, // Maximum of 2 secs
+// Event : specifying game
+type Event struct {
+	ID            string         `json:"id"`
+	Description   string         `json:"description"`
+	EventType     string         `json:"type"`
+	Link          string         `json:"link"`
+	Status        string         `json:"status"`
+	Sport         string         `json:"sport"`
+	StartTime     int            `json:"startTime"`
+	Live          bool           `json:"live"`
+	AwayTeamFirst bool           `json:"awayTeamFirst"`
+	DenySameGame  string         `json:"denySameGame"`
+	TeaserAllowed bool           `json:"teaserAllowed"`
+	CompetitionID string         `json:"competitionId"`
+	Notes         string         `json:"notes"`
+	NumMarkets    int            `json:"numMarkets"`
+	LastModified  int            `json:"lastModified"`
+	Competitors   []Competitor   `json:"competitors"`
+	DisplayGroups []DisplayGroup `json:"displayGroups"`
+}
+
+// DisplayGroup : specifying game
+type DisplayGroup struct {
+	ID            string   `json:"id"`
+	Description   string   `json:"description"`
+	DefaultType   bool     `json:"defaultType"`
+	AlternateType bool     `json:"alternateType"`
+	Markets       []Market `json:"markets"`
+	Order         int      `json:"order"`
+}
+
+// Competitor : specifying game
+type Competitor struct {
+	Home bool   `json:"home"`
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// Market : specifying game
+type Market struct {
+	ID           string    `json:"id"`
+	Description  string    `json:"description"`
+	Key          string    `json:"key"`
+	MarketTypeID string    `json:"marketTypeId"`
+	Status       string    `json:"status"`
+	SingleOnly   bool      `json:"singleOnly"`
+	Notes        string    `json:"notes"`
+	Period       Period    `json:"period"`
+	Outcomes     []Outcome `json:"outcomes"`
+}
+
+// Outcome : specifying game
+type Outcome struct {
+	ID           string `json:"id"`
+	Description  string `json:"description"`
+	Status       string `json:"status"`
+	OutcomeType  string `json:"outcomeType"`
+	CompetitorID string `json:"competitorId"`
+	Price        Price  `json:"price"`
+}
+
+// Price : specifying game
+type Price struct {
+	ID         string `json:"id"`
+	Handicap   string `json:"handicap"`
+	American   string `json:"american"`
+	Decimal    string `json:"decimal"`
+	Fractional string `json:"fractional"`
+	Malay      string `json:"malay"`
+	Indonesian string `json:"indonesian"`
+	Hongkong   string `json:"hongkong"`
+}
+
+// Period : specifying game
+type Period struct {
+	ID           string `json:"id"`
+	Description  string `json:"description"`
+	Abbreviation string `json:"abbreviation"`
+	Live         bool   `json:"live"`
+	Main         bool   `json:"main"`
+}
+
+// Path : specifying path
+type Path struct {
+	ID          string `json:"id"`
+	Link        string `json:"link"`
+	Description string `json:"description"`
+	PathType    string `json:"pathType"`
+	SportCode   string `json:"sportCode"`
+	Order       int    `json:"order"`
+	Leaf        bool   `json:"leaf"`
+	Current     bool   `json:"current"`
+}
+
+// ResultStruct : give json to this
+type ResultStruct struct {
+	Result []map[string]Competition
+}
+
+// ResultList : give json to this
+type ResultList struct {
+	ResultL []Competition
+}
+
+func main() {
+	res, err := http.Get("https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl")
+	if err != nil {
+		fmt.Println("1")
+		log.Fatal(err)
 	}
 
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	ret, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		fmt.Println("2")
+		log.Fatal(err)
+	}
+
+	retString := string(ret)
+	// marshErr := json.Unmarshal(ret, t)
+	dec := json.NewDecoder(strings.NewReader(retString))
+	var c []Competition
+	for {
+		if err := dec.Decode(&c); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", c[0].Events[0].ID)
+		fmt.Printf(string(len(c)))
+	}
+	json, err := json.MarshalIndent(c, "  ", "    ")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	req.Header.Set("User-Agent", "spacecount-tutorial")
+	fmt.Println(string(json))
 
-	res, getErr := spaceClient.Do(req)
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	body, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	people1 := people{}
-	jsonErr := json.Unmarshal(body, &people1)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-
-	fmt.Println(people1)
 }
-
